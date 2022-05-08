ba57cc1e05648dc5a887d3131a549988ee46a0d9
denchugo <tnkstr@live.jp>
2022-05-08 14:35:49 +0900

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/api/v1/quiz", function(req,res){
+    const answer = req.body.answer;
+    if(answer === "2"){
+        res.redirect("/correct.html");
+    } else {
+        res.redirect("/wrong.html");
+    }
+});
+
+app.listen(3000);
